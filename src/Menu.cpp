af7ea325494c1dfb28087aa0cb877a60ef8016b9
Shuai-Bee <beece-pm24@student.tarc.edu.my>
2025-12-19 01:47:52 +0800

    Add files via upload
    Signed-off-by: Shuai-Bee <beece-pm24@student.tarc.edu.my>

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <string>
+#include <omp.h>
 #include "jacobi_serial.h"
 #include "jacobi_omp.h"
 
@@ -21,17 +23,40 @@ void printTitle() {
 }
 
 int main() {
+    string input;
     int n, numThreads;
 	int maxIter = 5000;
     double tol = 1e-4;
 
     printTitle();
     // User input: matrix size, threads
-    cout << "Enter matrix size (e.g. 100, 300, 500): ";
-    cin >> n;
+    while (true) {
+        cout << "Enter matrix size (e.g. 100, 300, 500): ";
+        cin >> input;
+        
+        if (isdigit(input[0])) {
+            n = stoi(input);
+            break;
+        }
+        else {
+			cout << "Input must be digit only" << endl;
+        }
+	}
 
-    cout << "Enter number of threads (e.g. 1, 2, 4, 8): ";
-    cin >> numThreads;
+    while (true) {
+        cout << "Enter number of threads (e.g. 1, 2, 4, 8): ";
+        cin >> input;
+
+        if (isdigit(input[0])) {
+            numThreads = stoi(input);
+            break;
+        }
+        else {
+            cout << "Input must be digit only" << endl;
+        }
+    }
+   
+    vector<string> schedules = { "Static", "Dynamic", "Guided", "Auto", "Normal"};
 
     // Create matrix A and vector b
     vector<vector<double>> A(n, vector<double>(n));
@@ -40,14 +65,14 @@ int main() {
     // Generate diagonally dominant matrix
     // This ensures Jacobi method converges
     for (int i = 0; i < n; i++) {
-        double rowsum = 0.0;
-        for (int j = 0; j < n; j++) {
-            A[i][j] = rand() % 10 + 1;
-            if (i != j) rowsum += A[i][j];
-        }
-        // Increase diagonal value so that A[i][i] > sum of other elements
-        A[i][i] = rowsum + rand() % 5 + 5;
-        b[i] = rand() % 20 + 5;
+         double rowsum = 0.0;
+         for (int j = 0; j < n; j++) {
+             A[i][j] = rand() % 10 + 1;
+             if (i != j) rowsum += A[i][j];
+         }
+         // Increase diagonal value so that A[i][i] > sum of other elements
+         A[i][i] = rowsum + rand() % 5 + 5;
+         b[i] = rand() % 20 + 5;
     }
 
     // Run serial version
@@ -56,8 +81,11 @@ int main() {
 
     // Run OpenMP parallel version
     cout << "\nRunning OPENMP Jacobi..." << endl;
-    jacobi_omp(A, b, n, maxIter, tol, numThreads);
 
+        // Test different scheduling modes
+    for (const string& schedName : schedules) {
+        cout << "\n--- Mode: " << schedName << " ---" << endl;
+        jacobi_omp(A, b, n, maxIter, tol, numThreads, schedName);
+    }
 	return 0;
 }
-
--- src/jacobi_omp.cpp
+++ src/jacobi_omp.cpp
@@ -16,34 +16,71 @@ using namespace std;
 // b: constant vector
 // numThreads: number of threads to use
 // ---------------------------------------------------------------
-void jacobi_omp(vector<vector<double>>& A, vector<double>& b, int n, int maxIter, double tol, int numThreads)
+void jacobi_omp(vector<vector<double>>& A, vector<double>& b, int n, int maxIter, double tol, int numThreads,string typeName)
 {
     vector<double> x(n, 0.0), x_new(n, 0.0);
 
     // Set OpenMP thread count
     omp_set_num_threads(numThreads);
 
-    clock_t start = clock();  // start timing
+	double start = omp_get_wtime(); // start timing
 
     // Main iteration loop
+	// OMP parallel for with different scheduling types
     for (int iter = 0; iter < maxIter; iter++) {
-
-        // Parallel Jacobi update loop
-    #pragma omp parallel for
-        for (int i = 0; i < n; i++) {
-			double sigma = 0.0;             
-
-            // Compute sum of other terms in the row
-            for (int j = 0; j < n; j++) {
-                if (i != j) sigma += A[i][j] * x[j];
+        if (typeName == "Static") {
+        #pragma omp parallel for schedule(static)
+            for (int i = 0; i < n; i++) {
+                double sigma = 0.0;
+                for (int j = 0; j < n; j++) {
+                    if (i != j) sigma += A[i][j] * x[j];
+                }
+                x_new[i] = (b[i] - sigma) / A[i][i];
             }
-
-            x_new[i] = (b[i] - sigma) / A[i][i];
+        }
+        else if (typeName == "Dynamic") {
+        #pragma omp parallel for schedule(dynamic)
+            for (int i = 0; i < n; i++) {
+                double sigma = 0.0;
+                for (int j = 0; j < n; j++) {
+                    if (i != j) sigma += A[i][j] * x[j];
+                }
+                x_new[i] = (b[i] - sigma) / A[i][i];
+            }
+        }
+        else if (typeName == "Guided") {
+        #pragma omp parallel for schedule(guided)
+            for (int i = 0; i < n; i++) {
+                double sigma = 0.0;
+                for (int j = 0; j < n; j++) {
+                    if (i != j) sigma += A[i][j] * x[j];
+                }
+                x_new[i] = (b[i] - sigma) / A[i][i];
+            }
+        }
+        else if (typeName == "Auto") {
+        #pragma omp parallel for schedule(auto)
+            for (int i = 0; i < n; i++) {
+                double sigma = 0.0;
+                for (int j = 0; j < n; j++) {
+                    if (i != j) sigma += A[i][j] * x[j];
+                }
+                x_new[i] = (b[i] - sigma) / A[i][i];
+            }
+        }else{
+        #pragma omp parallel for
+            for (int i = 0; i < n; i++) {
+                double sigma = 0.0;
+                for (int j = 0; j < n; j++) {
+                    if (i != j) sigma += A[i][j] * x[j];
+                }
+                x_new[i] = (b[i] - sigma) / A[i][i];
+            }
         }
 
         // Parallel reduction to compute total error
         double error = 0.0;
-    #pragma omp parallel for reduction(+:error)
+        #pragma omp parallel for reduction(+:error)
         for (int i = 0; i < n; i++)
             error += fabs(x_new[i] - x[i]);
 
@@ -55,14 +92,15 @@ void jacobi_omp(vector<vector<double>>& A, vector<double>& b, int n, int maxIter
         x = x_new;
     }
 
-    clock_t end = clock();  // stop timing
-    double sec = (double)(end - start) / CLOCKS_PER_SEC;
+    double end = omp_get_wtime();  // stop timing
+    double sec = (end - start);
 
     cout << "\n----------------------------------\n";
     cout << "OpenMP\n";
     cout << "----------------------------------\n";
 	cout << "Matrix size: " << n << " x " << n << "\n";
     cout << "Number of threads: " << numThreads << "\n";
+    cout << "OpenMP type: " << typeName << "\n";
     cout << "Time completed: " << sec << " s\n";
     cout << "----------------------------------\n";
    
