7d87d71e1e8c97b2fab872e79ab85f727572c0f8
Shuai-Bee <beece-pm24@student.tarc.edu.my>
2025-12-19 01:19:20 +0800

    upadate back
    Replaced multiple scheduling types with a single parallel for loop.
    
    Signed-off-by: Shuai-Bee <beece-pm24@student.tarc.edu.my>

--- src/jacobi_omp.cpp
+++ src/jacobi_omp.cpp
@@ -28,47 +28,17 @@ void jacobi_omp(vector<vector<double>>& A, vector<double>& b, int n, int maxIter
     // Main iteration loop
 	// OMP parallel for with different scheduling types
     for (int iter = 0; iter < maxIter; iter++) {
-        if (typeName == "Static") {
-        #pragma omp parallel for schedule(static)
-            for (int i = 0; i < n; i++) {
-                double sigma = 0.0;
-                for (int j = 0; j < n; j++) {
-                    if (i != j) sigma += A[i][j] * x[j];
-                }
-                x_new[i] = (b[i] - sigma) / A[i][i];
-            }
-        }
-        else if (typeName == "Dynamic") {
-        #pragma omp parallel for schedule(dynamic)
-            for (int i = 0; i < n; i++) {
-                double sigma = 0.0;
-                for (int j = 0; j < n; j++) {
-                    if (i != j) sigma += A[i][j] * x[j];
-                }
-                x_new[i] = (b[i] - sigma) / A[i][i];
-            }
-        }
-        else if (typeName == "Guided") {
-        #pragma omp parallel for schedule(guided)
-            for (int i = 0; i < n; i++) {
-                double sigma = 0.0;
-                for (int j = 0; j < n; j++) {
-                    if (i != j) sigma += A[i][j] * x[j];
-                }
-                x_new[i] = (b[i] - sigma) / A[i][i];
-            }
-        }
-        else {
-        #pragma omp parallel for schedule(runtime)
-            for (int i = 0; i < n; i++) {
-                double sigma = 0.0;
-                for (int j = 0; j < n; j++) {
-                    if (i != j) sigma += A[i][j] * x[j];
-                }
-                x_new[i] = (b[i] - sigma) / A[i][i];
-            }
-        }
-
+       #pragma omp parallel for
+	    for (int i = 0; i < n; i++) {
+			double sigma = 0.0;             
+	
+	        // Compute sum of other terms in the row
+	        for (int j = 0; j < n; j++) {
+	            if (i != j) sigma += A[i][j] * x[j];
+	        }
+	
+	        x_new[i] = (b[i] - sigma) / A[i][i];
+	    }
         // Parallel reduction to compute total error
         double error = 0.0;
         #pragma omp parallel for reduction(+:error)
